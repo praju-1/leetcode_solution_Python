6af74f8c82ca3a716ad993ab6e18ec5e94e92518
Prajakt <prajaktatayde26@gmail.com>
2022-12-16 19:02:58 +0530

    Python program for return square root using math library

---
+++ sqrt_of_number/square_root.py
@@ -0,0 +1,19 @@
+"""
+Python program for return square root of given number
+
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. 
+The returned integer should be non-negative as well.
+"""
+import math
+try:
+    n = int(input("Enter number to return its square root :"))
+    def square_root_of_number(num : int):
+        "This function return the square root of the number"
+        try:
+            x = math.floor(math.sqrt(num))
+            print("\nSquare root of given number is : ", x)
+        except Exception as e:
+            print(e)
+    square_root_of_number(n)      
+except Exception as e:
+    print(e)
